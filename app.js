3495a7871be3e365af73a29c471736963bb46b25
Tal <talrasha007@gmail.com>
2022-05-05 20:49:42 +0800

    proxy implement

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const axios = require('axios');
 const Koa = require('koa');
 const KoaRouter = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 const router = new KoaRouter();
@@ -9,10 +11,12 @@ router.get('/hello', async (ctx) => {
 });
 
 router.post('/query', async (ctx) => {
-  ctx.body = { foo: 1 };
+  const { header: { authorization }, query: { url }, request: { body: payload } } = ctx;
+  const { data } = await axios.post(url + '/api/ds/query', payload, { headers: { Authorization: authorization } });
+  ctx.body = data;
 });
 
-app.use(router.allowedMethods()).use(router.routes());
+app.use(bodyParser()).use(router.allowedMethods()).use(router.routes());
 app.listen(9000, () => {
   console.log(`Server start on http://localhost:9000`);
 });
