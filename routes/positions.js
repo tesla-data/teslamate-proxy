d7b8e66d7bfc124803965859b5e9f313927b34e3
Tal <talrasha007@gmail.com>
2022-05-26 19:27:50 +0800

    return raw data for big results.

--- routes/positions.js
+++ routes/positions.js
@@ -4,5 +4,5 @@ const position = require('../lib/position');
 module.exports = async (ctx) => {
   const { header: { authorization }, query: { url, car_id, from = Date.now() - 86400 * 1000, to = Date.now() } } = ctx;
   const query = new Query(url, authorization);
-  ctx.body = await query.execute([position.buildQuery(car_id, from, to)], from, to);
+  ctx.body = await query.execute([position.buildQuery(car_id, from, to)], from, to, false);
 };
