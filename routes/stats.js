6245743de8658f50b147ee0dcea6f85b36ad6be5
Tal <talrasha007@gmail.com>
2022-06-01 13:46:41 +0800

    stats query for period

--- routes/stats.js
+++ routes/stats.js
@@ -2,7 +2,7 @@ const Query = require('../lib/Query');
 const stats = require('../lib/stats');
 
 module.exports = async (ctx) => {
-  const { header: { authorization }, query: { url, car_id } } = ctx;
+  const { header: { authorization }, query: { url, car_id, period = 'month', from = '0', to = Date.now() } } = ctx;
   const query = new Query(url, authorization);
-  ctx.body = await query.execute([stats.buildQuery(car_id)]);
+  ctx.body = await query.execute([stats.buildQuery(car_id, period)], from, to);
 };
