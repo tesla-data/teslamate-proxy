50d72548b11f613a9de23c56d3f2e6d300fd9f41
Tal <talrasha007@gmail.com>
2022-10-25 21:33:20 +0800

    add car_id param

--- routes/charge_detail.js
+++ routes/charge_detail.js
@@ -2,7 +2,7 @@ const Query = require('../lib/Query');
 const chargeDetail = require('../lib/chargeDetail');
 
 module.exports = async (ctx) => {
-  const { header: { authorization }, query: { url, charge_id, from, to } } = ctx;
+  const { header: { authorization }, query: { url, charge_id, car_id, from, to } } = ctx;
   const query = new Query(url, authorization);
-  ctx.body = await query.execute(chargeDetail.buildQuery(charge_id), from, to);
+  ctx.body = await query.execute(chargeDetail.buildQuery(charge_id, car_id), from, to);
 };
