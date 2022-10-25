6d72ef548fb5b45d6fd02e518e5f74be90ec0b3b
Tal <talrasha007@gmail.com>
2022-10-25 21:39:29 +0800

    update charge share.

--- routes/share/charge.js
+++ routes/share/charge.js
@@ -3,12 +3,12 @@ const Query = require('../../lib/Query');
 const charges = require('../../lib/charges');
 const chargeDetail = require('../../lib/chargeDetail');
 
-async function saveCharge({ authorization, url, car_id, charge_id }) {
+async function saveCharge({ authorization, url, car_id, charge_id, from, to }) {
   const jsonFile = new JsonFile('/share/charge', url + authorization, charge_id);
 
   if (!jsonFile.exists()) {
     const query = new Query(url, authorization);
-    const [detail] = await query.execute(chargeDetail.buildQuery(charge_id), Date.now(), Date.now());
+    const [detail] = await query.execute(chargeDetail.buildQuery(charge_id, car_id), from, to);
     const [charge] = await query.execute([charges.buildQuery(car_id)], detail[0].time, detail[1].time);
     jsonFile.save({ detail, charge });
   }
@@ -17,8 +17,8 @@ async function saveCharge({ authorization, url, car_id, charge_id }) {
 }
 
 module.exports = async (ctx) => {
-  const { header: { authorization }, query: { url, car_id, charge_id } } = ctx;
-  const { hash, id } = await saveCharge({ authorization, url, car_id, charge_id });
+  const { header: { authorization }, query: { url, car_id, charge_id, from, to } } = ctx;
+  const { hash, id } = await saveCharge({ authorization, url, car_id, charge_id, from, to });
   ctx.body = { path: '/share/charge', hash, id };
 };
 
