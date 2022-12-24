cad572890029baa4cdc6a2d140330160c863ee87
Tal <talrasha007@gmail.com>
2022-12-24 14:57:55 +0800

    use aliyun

--- routes/teslafi.js
+++ routes/teslafi.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 
 module.exports = async (ctx) => {
-  ctx.body = JSON.parse(fs.readFileSync('/mnt/teslafi_firmware.json'));
+  ctx.body = JSON.parse(fs.readFileSync('/data/teslafi_firmware.json'));
 };
